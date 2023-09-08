a1deddc27a7a32ee73daf75210ce13c19eee4200
thompa21 <tholind@kth.se>
2023-09-08 09:19:48 +0200

    info text custom html rum

--- src/edit_entry.php
+++ src/edit_entry.php
@@ -1893,6 +1893,14 @@ foreach ($edit_entry_field_order as $key)
   }
 }
 
+
+//KTH custom_html(information text i edit room)
+
+$room_custom_html = ""
+$room_custom_html = get_room_custom_html($room_id,$lang);
+if ($room_custom_html != "") {
+	echo "<div id=\"extrainfo\" style=\"\">" . $room_custom_html ."</div>";
+}
 //KTH Extra info(röd ruta om speciell info... bokningsregler eller dylikt)
 //191003
 /*
